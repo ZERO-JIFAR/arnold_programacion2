8d795386655dbc7c22691779fd56a6098aae5559
ZERO-JIFAR <juanignacioarnold@gmail.com>
2024-04-22 22:07:32 -0300

    arreglos clase 9-4 Unidi

--- ejerClase_9-4/src/AsoUnidireccional/Main.java
+++ ejerClase_9-4/src/AsoUnidireccional/Main.java
@@ -1,4 +1,12 @@
 package AsoUnidireccional;
 
 public class Main {
+    public static void main(String[] args) {
+        University universidad1 = new University("UTN");
+        University universidad2 = new University("FCEN");
+        Student estudiante1 = new Student("Carlod", universidad2);
+        estudiante1.seeUniversity();
+        estudiante1.changeUniversity(universidad1);
+        estudiante1.seeUniversity();
+    }
 }
--- ejerClase_9-4/src/AsoUnidireccional/Student.java
+++ ejerClase_9-4/src/AsoUnidireccional/Student.java
@@ -1,4 +1,32 @@
 package AsoUnidireccional;
 
 public class Student {
+    private String name;
+    private University university;
+
+    public Student(String name, University university) {
+        this.name = name;
+        this.university = university;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+    public void seeUniversity(){
+        System.out.println("Universidad actual de "+getName()+": "+getUniversity().getName());
+    }
+    public void changeUniversity(University newUniversity){
+        setUniversity(newUniversity);
+        System.out.println(getName()+" cambio de universidad a "+getUniversity().getName());
+    }
 }
