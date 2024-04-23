d85501b1596f981e2639729b7de0f75efc5044f7
ZERO-JIFAR <juanignacioarnold@gmail.com>
2024-04-22 22:26:49 -0300

    arreglos clase 9-4 Compo

--- ejerClase_9-4/src/Composicion/Door.java
+++ ejerClase_9-4/src/Composicion/Door.java
@@ -1,4 +1,16 @@
 package Composicion;
 
 public class Door {
+    private String material;
+    public Door(String material){
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
--- ejerClase_9-4/src/Composicion/Main.java
+++ ejerClase_9-4/src/Composicion/Main.java
@@ -1,4 +1,8 @@
 package Composicion;
 
 public class Main {
+    public static void main(String[] args) {
+        House casa1 = new House(5);
+        casa1.seeMaterial();
+    }
 }
